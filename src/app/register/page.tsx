1a6a884ffa2b551c320eae2228610492175dca2d
devsanbid <devsanbid@gmail.com>
2025-02-23 13:11:39 +0545

    added register page UI

---
+++ src/app/register/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/main/Footer";
+import Navbar from "@/components/main/Navbar";
+import RegisterForm from "@/components/main/RegisterFrame";
+
+export default function Register() {
+	return (
+		<div>
+            <Navbar />
+			<RegisterForm />
+            <Footer />
+		</div>
+	);
+}
